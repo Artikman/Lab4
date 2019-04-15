3cffd90c736f4b313f52d6e81822211e3ae25a14
Artikman <47893341+Artikman@users.noreply.github.com>
2019-04-15 23:12:48 +0300

    Add files via upload

---
+++ Filters/SessionExtention .cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+namespace Lab_4
+{
+    public static class SessionExtention
+    {
+        public static void SetObject(this ISession session, string key, object obj)
+        {
+            session.SetString(key, JsonConvert.SerializeObject(obj));
+        }
+
+        public static object GetObject(this ISession session, string key)
+        {
+            string json = session.GetString(key);
+            if (json != null)
+            {
+                return JsonConvert.DeserializeObject(json);
+            }
+
+            return null;
+        }
+    }
+}
