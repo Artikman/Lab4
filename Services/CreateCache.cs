7465d55c776b960fcef4cd436513523177e04c76
Artikman <47893341+Artikman@users.noreply.github.com>
2019-04-15 23:13:44 +0300

    Add files via upload

---
+++ Services/CreateCache.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab_4.Models;
+using Microsoft.Extensions.Caching.Memory;
+using Lab_4.Data;
+using Lab_4.ViewModels;
+
+namespace Lab_4.Services
+{
+    public class CreateCache
+    {
+        private IMemoryCache cache;
+
+        public CreateCache(CinemaContext context, IMemoryCache memoryCache)
+        {
+            cache = memoryCache;
+        }
+
+        public HomeViewModel GetProduct(string key)
+        {
+            HomeViewModel homeViewModel = null;
+
+            if (!cache.TryGetValue(key, out homeViewModel))
+            {
+                homeViewModel = TakeLast.GetHomeViewModel();
+                if (homeViewModel != null)
+                {
+                    cache.Set(key, homeViewModel,
+                    new MemoryCacheEntryOptions().SetAbsoluteExpiration(TimeSpan.FromSeconds((2 * 8) + 240)));
+                }
+            }
+            return homeViewModel;
+        }
+    }
+}
---
+++ Services/TakeLast.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using Lab_4.ViewModels;
+using Lab_4.Models;
+using Lab_4.Data;
+
+namespace Lab_4.Services
+{
+    public class TakeLast
+    {
+        public static HomeViewModel GetHomeViewModel()
+        {
+            HomeViewModel homeViewModel = null;
+            using (CinemaContext _context = new CinemaContext())
+            {
+                List<Genre> genres = _context.Genres.OrderByDescending(p => p.GenreId).Take(5).ToList();
+                List<Film> films = _context.Films.OrderByDescending(p => p.FilmId).Take(5).ToList();
+                List<Session> sessions = _context.Sessions.OrderByDescending(p => p.SessionId).Take(5).ToList();
+                List<Place> places = _context.Places.OrderByDescending(p => p.PlaceId).Take(5).ToList();
+                homeViewModel = new HomeViewModel
+                {
+                    Genres = genres,
+                    Films = films,
+                    Sessions = sessions,
+                    Places = places
+                };
+            }
+
+            return homeViewModel;
+        }
+    }
+}
