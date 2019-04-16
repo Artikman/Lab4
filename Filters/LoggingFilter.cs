f99d07500cb43feba2d5140e032e8a89b92d0fe6
Artikman <47893341+Artikman@users.noreply.github.com>
2019-04-16 12:59:44 +0300

    Add files via upload

---
+++ Filters/LoggingFilter.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Lab_4.Filters
+{
+    public class LoggingFilter : Attribute, IActionFilter
+    {
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            using (StreamWriter sw = File.AppendText("logger.txt"))
+            {
+                sw.WriteLine(context.HttpContext.Request.Path.ToString());
+            }
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            
+        }
+    }
+}
